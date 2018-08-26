743ab89f87b623a6a2eb9107b6d33ef49ed766ca
Mariia/Chernetska <mariia.chernetska90@gmail.com>
2018-08-26 16:22:41 +0300

    changing 2

--- Module-1/js/index.js
+++ Module-1/js/index.js
@@ -1,85 +1,5 @@
-// 'use strict';
-// function power (prop){
-//     return prop**2;
-// };
-// console.log(power(5));
-// let a;
-// a = 5;
-// const b = 5;
+'use strict';
 
-
-
-
-// const name = 'Mango';
-// const date = '14/08/2031';
-// const roomType = 'люкс';
-
-// const message = `${name} прибывает на отдых ${date} в ${roomType}`;
-// console.log(message);
-
-
-// const padding = "20px";
-// const border = "5px";
-// const contentWidth = "100px";
-
-// let totalWidth = Number.parseInt(padding) + Number.parseInt(border) + Number.parseInt(contentWidth) + 'px';
-
-
-// console.log(totalWidth); // '125px'
-
-
-
-
-
-// let messageAs;
-
-// const year = prompt('Какой сейчас год?');
-
-// if (parsetInt(year) === "2018") {
-//     messageAs = 'Все верно!';
-// }
-// else {
-//     messageAs = 'Но ведь на вдоре 2018!';   
-// } 
-// alert(messageAs);
-
-
-
-
-// const a = 20;
-// const result = a < 30 || a > 10;
-// console.log(result)
-
-// const power = 32;
-// const power2 = 10000;
-
-
-
-// const calculation = prompt('Чему равно 2**5?'); //2**5 привидения в степень 2*2*2*2*2
-// if (Number(calculation) === power){
-//     //(calculation === power)
-//     //if (Number.(calculation === 4))
-//     alert('Умничка!');
-// } else if (!calculation){ //(calculation === null)
-//     alert('mistake');9
-// } else{
-//     alert('please try again');
-// }
-
-//const date = formattyDay + "\\" + "\\"  + "0"
-
-
-
-// const $number = prompt ('Enter number!');
-
-// }if ($number === null){
-//     alert ('no!');
-// } else{
-//     if (parseInt($number) !== (Number($number)){
-//         alert('Enter celoe chyslo');
-//     }else{
-//     alert ('Thanks!');
-// }
 
 // ===========================================================================
 
@@ -88,17 +8,17 @@
 
 
 
-// const adminLogin = 'admin';
-// const adminPassword = 'm4ngo1zh4ackz0r';
+const adminLogin = 'admin';
+const adminPassword = 'm4ngo1zh4ackz0r';
 
-// let messageFirst = 'Отменено пользователем!';
-// let messageSecond = 'Доступ запрещен!';
-// let messageThird = 'Добро пожаловать!';
+let messageFirst = 'Отменено пользователем!';
+let messageSecond = 'Доступ запрещен!';
+let messageThird = 'Добро пожаловать!';
 
-// const userLogin = prompt('Введите Ваш Логин!');
+const userLogin = prompt('Введите Ваш Логин!');
 
 
-// //task 1 check login
+
 if(userLogin === adminLogin){ // this is good
     //task 2
     const userPassword = prompt('Enter the password');
@@ -135,33 +55,3 @@ if (!userLogin){
   } else {
     alert(message2);
   }
-
-// ==========================================================================
-
-// number1 < x < number2
-
-
-// const groupSharm = 15;
-// const groupHurgada = 25;
-// const groupTaba = 6;
-
-// const numberPax = prompt('Введите Количество мест!');
-// if(!Number(numberPax)){
-//     alert ('Ошибка ввода!');
-// }
-// else{
-//   if(numberPax <= groupTaba && numberPax > 0 ){
-//      TABA
-//      }
-//      else if(numberPax <= groupTaba && numberPax > 0 ){
-//        const correctNumberPax = confirm ('У нас есть место  в этой группе, согласны ли Вы быть в этой группе?'); 
-//       }
-//       else if (correctNumberPax = 'да'){
-//             alert ('Приятного путешествия в группе Taba');
-//      } else if (correctNumberPax = 'нет'){
-//              alert ('Нам очень жаль, приходите еще!');
-//          }
-//      }
-       
-// }
-
